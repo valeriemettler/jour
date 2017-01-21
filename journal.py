78b0fae1acd2e7f7ffd52e8bbb52f35635fcebb7
Valerie Mettler <valerie.mettler@swirl.net>
2017-01-21 13:00:42 -0800

    added sign up feature

--- journal.py
+++ journal.py
@@ -28,6 +28,7 @@ def login():
     <input type="password" name="password" />
     <input type="submit">
     </form>
+    <p><a href="/signup">Sign up</a> for an account.</p>
     </body>
     </html>
     """.format()
@@ -52,19 +53,8 @@ def login_action():
         <a href="/login">Return to Login</a>
         """
 
-
-
-@app.route("/journal/<username>")
-def render_journal(username):
-    text_file = open("journaldata.txt", "r")
-    curr_data = json.loads(text_file.read())
-    text_file.close()
-    print "username is ", username
-    print curr_data
-    show_data = curr_data[username]
-    x = ""
-    for i in show_data:
-        x = x + '<p class='"todo"'>{}</p>'.format(i)
+@app.route("/signup")
+def signup():
     return """
     <!DOCTYPE html>
     <html>
@@ -74,21 +64,94 @@ def render_journal(username):
     <link rel="stylesheet" type="text/css" href="./journal.css">
     </head>
     <body>
-    <p>Welcome to your Journal!</p>
-    <form method="get" action="/update_action">
-    <input type="text" name="journal_input" />
-    <input type="text" name="username" value="{0}" hidden="true" />
+    <p>Sign up for an account here.</p>
+    <p>Choose a Username and Password</p>
+    <form method="get" action="/signup_action">
+    <input type="text" name="username" />
+    <input type="password" name="password" />
     <input type="submit">
     </form>
-    <div>{1}</div>
-    </form>
+    <p>Already have an account? <a href="/login_action">Log in.</a></p>
     </body>
     </html>
-    """.format(username,x)
+    """
+
+@app.route("/signup_action")
+def signup_action():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    x = read_data()
+    if username in x.keys():
+        return """
+        <p>There is already an account with that username</p>
+        <p><a href="/signup">Sign up</a> with a different username or <a href="/login">return to Login</a></p>
+        """
+    else:
+        #new_user = x[username]
+        x[username] = password
+        #return json.dumps(x)
+
+        text_file = open("user.txt", "w")
+        output = json.dumps(x)
+        text_file.write(output)
+        text_file.close()
+        return redirect("/login")
+
+
+@app.route("/journal/<username>")
+def render_journal(username):
+    text_file = open("journaldata.txt", "r")
+    curr_data = json.loads(text_file.read())
+    text_file.close()
+    print "username is ", username
+    print curr_data
+    if not username in curr_data.keys():
+            return """
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <meta charset="UTF-8">
+            <title>Jour</title>
+            <link rel="stylesheet" type="text/css" href="./journal.css">
+            </head>
+            <body>
+            <p>Welcome to your Journal!</p>
+            <form method="get" action="/update_action">
+            <input type="text" name="journal_input" />
+            <input type="text" name="username" value="{0}" hidden="true" />
+            <input type="submit">
+            </form>
+            </body>
+            </html>
+            """.format(username)
+    else:
+        show_data = curr_data[username]
+        x = ""
+        for i in show_data:
+            x = x + '<p class='"todo"'>{}</p>'.format(i)
+        return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Jour</title>
+        <link rel="stylesheet" type="text/css" href="./journal.css">
+        </head>
+        <body>
+        <p>Welcome to your Journal!</p>
+        <form method="get" action="/update_action">
+        <input type="text" name="journal_input" />
+        <input type="text" name="username" value="{0}" hidden="true" />
+        <input type="submit">
+        </form>
+        <div>{1}</div>
+        </body>
+        </html>
+        """.format(username,x)
 
 @app.route("/journal/journal.css")
 def render_css():
-    #put css in a file, read the file and then send it (instead of having it inline in the program)
+    #put css in a file, read the file and then send it
     text_file = open("journal.css", "r")
     curr_css = text_file.read()
     text_file.close()
@@ -103,16 +166,29 @@ def update_journal():
     text_file = open("journaldata.txt", "r")
     curr_data = json.loads(text_file.read())
     text_file.close()
-    curr_array = curr_data[username]
-    print curr_array
-    # insert journal_input into curr_data to make new data
-    curr_array.append(journal_input)
-    new_data = curr_data
-    text_file = open("journaldata.txt", "w")
-    output = json.dumps(new_data)
-    text_file.write(output)
-    text_file.close()
-    return redirect("/journal/{0}".format(username))
+    if not username in curr_data.keys():
+        curr_data[username] = []
+        curr_array = curr_data[username]
+        print curr_array
+        # insert journal_input into curr_data to make new data
+        curr_array.append(journal_input)
+        new_data = curr_data
+        text_file = open("journaldata.txt", "w")
+        output = json.dumps(new_data)
+        text_file.write(output)
+        text_file.close()
+        return redirect("/journal/{0}".format(username))
+    else:
+        curr_array = curr_data[username]
+        print curr_array
+        # insert journal_input into curr_data to make new data
+        curr_array.append(journal_input)
+        new_data = curr_data
+        text_file = open("journaldata.txt", "w")
+        output = json.dumps(new_data)
+        text_file.write(output)
+        text_file.close()
+        return redirect("/journal/{0}".format(username))
 
 
 def read_data():
